d741cf643f333ec1c8c86afa43f68a52959f0d51
Ashkan Alizadeh <a3hkanalizadeh@gmail.com>
2026-04-24 01:07:54 +0330

    Update basic calculator

--- mini-projects/basic-calculator.py
+++ mini-projects/basic-calculator.py
@@ -1,25 +1,24 @@
+from colorama import Fore, Style, init
+
+init()
+
 try:
-    num1 = int(input("Enter your number 1: "))
-    num2 = int(input("Enter your number 2: "))
-    op = input("Enter your operation: ")
-
-    if op == "+":
-        print(num1 + num2)
-
-    elif op == "-":
-        print(num1 - num2)
-
-    elif op == "*":
-        print(num1 * num2)
-
-    elif op == "/":
-        if num2 == 0:
-            print("I can't divide by zero")
-        else:
-            print(num1 / num2)
-
-    else:
-        print("Enter a valid operation")
-
+    num1 = float(input(Fore.CYAN+"Enter first number: "+Style.RESET_ALL))
+    num2 = float(input(Fore.CYAN+"Enter second number: "+Style.RESET_ALL))
+    op = input(Fore.YELLOW+"Enter your operation: "+Style.RESET_ALL)
+    match op:
+        case "+":
+            print(f"Result: {num1 + num2}")
+        case "-":
+            print(f"Result: {num1 - num2}")
+        case "*":
+            print(f"Result: {num1 * num2}")
+        case "/":
+            if num2 == 0:
+                print(Fore.RED + "You can't divide by zero!"+Style.RESET_ALL)
+            else:
+                print(f"Result: {num1 / num2}")
+        case _:
+            print(Fore.RED + "Enter a valid operation!"+Style.RESET_ALL)
 except ValueError:
-    print("Please enter valid numbers")
+    print(Fore.RED + "Please enter valid numbers"+Style.RESET_ALL)
